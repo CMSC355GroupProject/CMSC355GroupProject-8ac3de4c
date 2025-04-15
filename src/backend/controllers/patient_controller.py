d9e195e6ceb91eb93a5bb21b70cf8ed25e82dc53
JoeyVCU <fondacoj@vcu.edu>
2025-04-15 14:37:49 -0400

    Update patient_controller.py

--- src/backend/controllers/patient_controller.py
+++ src/backend/controllers/patient_controller.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 from bson.objectid import ObjectId
 from app import mongo
 from models.patient import format_patient
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 
 def get_all_patients():
     patients = mongo.db.patients.find()
@@ -13,8 +13,6 @@ def create_patient():
     new_id = mongo.db.patients.insert_one(data).inserted_id
     new_patient = mongo.db.patients.find_one({"_id": new_id})
     return jsonify(format_patient(new_patient)), 201
-
-from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt
 
 @jwt_required()
 def get_current_patient():
